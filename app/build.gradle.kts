893911ac2597437755b0500a240e72d4b5f9352b
Agustinpujol12 <agustinpujol12@gmail.com>
2025-09-29 12:17:51 -0300

    chore(release): bump version to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         targetSdk = 36
 
         // Cambiá estos cuando quieras instalar por encima de otra versión
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
